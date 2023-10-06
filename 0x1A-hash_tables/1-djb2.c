04921a361557fb51d1ae818cb13876b94a607843
derick mokua <derickmokua@outlook.com>
2023-10-05 21:06:49 -0700

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Computes the hash using the djb2 algorithm
+ *
+ * @str: A pointer to the string to be hashed
+ *
+ * Return: The computed hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
